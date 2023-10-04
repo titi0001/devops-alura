0dc153d32326af881607ba416838ff78eb71b89e
Thiago Pereira <titiura@gmail.com>
2023-10-04 10:07:52 -0300

    feat: processos_memoria.sh

--- ShellScripting/processos_memoria.sh
+++ ShellScripting/processos_memoria.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
+if [ ! -d log ]
+then 
+  mkdir log
+fi
 
+processos_memoria(){
 processos=$(ps -e -o pid --sort -size | head -n 11 | awk '$1 - /^[0-9]+$/')
 for pid in $processos
   do
-   echo  $(ps -p $pid -o comm=)
+   nome_processo=$(ps -p $pid -o comm=)
+   echo -n $(date  +%F,%H:%M:%S, ) >> $nome_processo.log
+   tamanho_processo=$(ps -p $pid -o size | awk '$1 - /^[0-9]+$/')
+   echo '$(bc <<< "scale=2;$tamanho_processo/1024") MB' >> log/$nome_processo.log
 done 
+}
 
+processos_memoria
+if [ $? -eq 0 ]
+then
+  echo "Os arquivos foram salvos com sucesso"
+else
+ echo "Houve um problema na hora de salvar os arquivos"
+fi
